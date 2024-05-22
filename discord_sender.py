8049343b84044d76a081a2092e2781058be70872
andy0000-droid <andy020207@gmail.com>
2024-05-22 22:22:33 +0900

    code update

--- discord_sender.py
+++ discord_sender.py
@@ -1,10 +1,12 @@
 import json
 import requests
-
+from datetime import datetime
 
 webhook_url = "https://discord.com/api/webhooks/1242492003854323833/rhy9ugLaGzCE8_NkT-7VWuN37L2LOrbPfaDd4Tkg0PlPITpkxNM58VA-rCRBcwR1pQbG"
 def send_message(text: str):
     headers = {'Content-Type': 'application/json'}
+    nowtime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    text = "Time: {}\n{}".format(nowtime, text)
     payload = json.dumps({'content': text})
     r = requests.post(url=webhook_url, data=payload, headers=headers)
 
--- training_code.py
+++ training_code.py
@@ -2,7 +2,8 @@ from langchain_community.llms import Ollama
 import transformers
 import torch
 llm = Ollama(model="llama3:8b-instruct")
-model_id = ["meta-llama/Meta-Llama-3-8B-Instruct", "meta-llama/Meta-Llama-3-70B-Instruct"]
+#model_id = ["meta-llama/Meta-Llama-3-8B-Instruct", "meta-llama/Meta-Llama-3-70B-Instruct"]
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
 import os
 os.environ["HF_TOKEN"]="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"
@@ -17,10 +18,25 @@ if not(os.path.isdir(result_dir)):
     os.mkdir(result_dir)
 
 from discord_sender import send_message
+from datetime import datetime
+from time import sleep
+
+def alive(_worker_id):
+    second = 1
+    minute = 60 * second
+    hour = 60 * minute
+    while True:
+        sleep(30 * second)
+        send_message("worker_{} is alive".format(_worker_id))
+
+from multiprocessing import Process
 
 def model(_model_id, _worker_id, datasets):
-    _model = _model_id[(_worker_id % 2)]
-    print("START: {} from worker_{}".format(_model, _worker_id))
+    proc = Process(target=alive, args=(_worker_id, ))
+    proc.start()
+    _model = _model_id
+    print("START: {} from worker_{}\nPID: {}".format(_model, _worker_id, os.getpid()))
+    nowtime = datetime.now().strftime("%Y %m %d %H:%M:%S")
     send_message("START: {} from worker_{}".format(_model, _worker_id))
 
     start = time()
@@ -30,11 +46,18 @@ def model(_model_id, _worker_id, datasets):
         model_kwargs={"torch_dtype": torch.bfloat16},
         device_map="auto",
     )
-    messages = [
-        {"role": "system", "content": "You are a Opensource expert who can "},
-        {"role": "system", "content": datasets['train']},
-        {"role": "user", "content": "Where is this code from"+datasets['train'][0]['instruction']}
-    ]
+    message = None
+    if _worker_id % 2 == 0:
+        messages = [
+            {"role": "system", "content": "You are a Opensource expert who can "},
+            {"role": "user", "content": datasets['train']},
+            {"role": "user", "content": "Where is this code from"+datasets['train'][0]['instruction']}
+        ]
+    else:
+        messages = [
+            {"role": "system", "content": "You are a Opensource expert who can "},
+            {"role": "user", "content": "Where is this code from"+datasets['train'][0]['instruction']}
+        ]
     prompt = pipeline.tokenizer.apply_chat_template(
             messages, 
             tokenize=False, 
@@ -53,24 +76,17 @@ def model(_model_id, _worker_id, datasets):
         top_p=0.9,
     )
     end = time()
+    proc.kill()
+    proc.join()
     print("FINISH: {} from worker{}".format(_model, _worker_id))
-    send_message("FINISH: {} from worker_{}. Execution Time: {}".format(_model, _worker_id, end - start))
-    yield(outputs[0]["generated_text"][len(prompt):], end - start, _model)
+    send_message("FINISH: {} from worker_{}.\nExecution Time: {}".format(_model, _worker_id, end - start))
+    yield(outputs[0]["generated_text"][len(prompt):], end - start, _model, nowtime)
 
 result_file = "Result"
 ext = ".txt"
 
-from multiprocessing import Process
-from time import sleep
-def alive(_worker_id):
-    sleep(600)
-    send_message("worker_{} is alive".format(_worker_id))
-
 def trainer(worker_id, datasets):
-    
-    proc = Process(target=alive, args=(worker_id, ))
-    proc.start()
-    for result, execution_time, used_model in model(model_id, worker_id, datasets):
+    for result, execution_time, used_model, start_time in model(model_id, worker_id, datasets):
         
         res_dict = dict()
         res_dict['Result'] = result
@@ -78,5 +94,4 @@ def trainer(worker_id, datasets):
         result_path = os.path.join(result_dir, result_file+str(worker_id)+ext)
 
         with open(result_path, 'a') as f:
-            f.write("Model: {}\nTime: {}\nResult: {}\n\n\n".format(used_model, res_dict['Time'], res_dict['Result']))
-        proc.join()
+            f.write("Start at: {}\nModel: {}\nTime: {}\nResult: {}\n\n\n".format(start_time, used_model, res_dict['Time'], res_dict['Result']))
